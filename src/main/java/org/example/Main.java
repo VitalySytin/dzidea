4a66a36097f2ed629a0563db3bab0a734353e4db
VSytin <git.user.faloself@gmail.com>
2024-07-14 10:48:47 +0300

    Изменено количество сообщений

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
             System.out.println("Hello world!22");
-            System.out.println("1234);
+            System.out.println("1234");
                     System.out.println("4321");
             System.out.println("5555");
 
